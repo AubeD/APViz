7306571e078bb29e87f663d1b4e931e25d6a3146
AubeD <o.bs@hotmail.fr>
2019-02-15 17:33:00 +0100

    Update graph2.js
    Ajout température en nuage de point

--- graph2.js
+++ graph2.js
@@ -16,23 +16,24 @@ var svg2 = d3.select("#graph2").append("svg")
   // Scales  
   var y2 = d3.scaleLinear()
     .range([imgHeight2, 0]);
+
+  var y_temp = d3.scaleLinear()
+    .range([imgHeight2, 0]);
+
   
   var t2 = d3.scaleTime()
     .range([0, imgWidth2]);
 
   // Date format https://bl.ocks.org/zanarmstrong/ca0adb7e426c12c06a95
-  var parseTime  = d3.timeParse("%Y-%m-%d %H:%M:%S.000")
+  var parseTime  = d3.timeParse("%Y-%m-%d %H:%M:%S.000");
   
-  var color = d3.scaleOrdinal(d3.schemeCategory20) 
+  var color = d3.scaleOrdinal(d3.schemeCategory20);
 
 
 
   // Load stocks data
   // Ex: 0: {symbol: "MSFT", date: "Jan 2000", price: "39.81"}
   d3.csv('https://raw.githubusercontent.com/AubeD/APViz/master/exercise.csv', function(error, raw2) {
-    //console.log(raw)
-    //var symbols = [];
-    console.log(imgHeight2);
 
     // Data pre-processing
     raw2.forEach(function(d, i) {
@@ -47,21 +48,50 @@ var svg2 = d3.select("#graph2").append("svg")
       //console.log(d.date);
     });
 
+    d3.csv('https://raw.githubusercontent.com/AubeD/APViz/master/weather-lyon.csv', function(error, meteo_lyon) {
+    console.log(meteo_lyon);
+    // Data pre-processing
+      meteo_lyon.forEach(function(d, i) {
+        
+        // String to INT
+
+        d.temp = +d.Temperature-272.15;     
+   
+        // Parsing time
+        
+        d.date = new Date(d.Date);
+        if(d.temp<-10){
+        console.log(d.temp);
+        }
+        //console.log(d.date);
+        if(d.Temperature==0){
+          meteo_lyon.splice(meteo_lyon.indexOf(d), 1);
+        }
+      });
+
 
     
     // Calculated on the flat dataset
-    t2.domain(d3.extent(raw2, function(d) {
-      return d.date;
-    }))
+    var date_min = d3.min(raw2, function(d) {return d.date;});
+    var date_max = d3.max(meteo_lyon, function(d){return d.date;});
+    t2.domain([date_min,date_max]);
     
     y2.domain([0, d3.max(raw2, function(d) {
       return d.value;
-    })])
+    })]);
+
+    y_temp.domain([0, d3.max(meteo_lyon, function(d) {
+      return d.temp;
+    })]);
 
     function sortByDateAscending(a, b) {
         // Dates will be cast to numbers automagically:
         return a.date - b.date;
     }
+
+    meteo_lyon = meteo_lyon.sort(sortByDateAscending);
+
+    //console.log(meteo_lyon);
 
     raw2 = raw2.sort(sortByDateAscending);
     
@@ -71,11 +101,19 @@ var svg2 = d3.select("#graph2").append("svg")
       .tickFormat(displayDate);
     var yAxis = d3.axisLeft()
       .scale(y2);
+    var yAxis_temp = d3.axisLeft()
+      .scale(y_temp);
     
     // Line generator
     var line = d3.line()//.curve(d3.curveCardinal)
+      .defined(function(d) {return d.date < date_max && d.date > date_min;})
       .x(function(d) { return t2(d.date); }) // Update X mapping
       .y(function(d) { return y2(d.value); }) // Update Y mapping
+
+    var line_temp = d3.line()//.curve(d3.curveCardinal)
+      .defined(function(d) {return (d.date < date_max && d.date > date_min);})
+      .x(function(d) { return t2(d.date); }) // Update X mapping
+      .y(function(d) { return y_temp(d.temp); }) // Update Y mapping
 
       //console.log(raw);
 
@@ -88,9 +126,33 @@ var svg2 = d3.select("#graph2").append("svg")
       .attr("stroke", color(1))
       .attr("fill", "none");
 
-    var line2 = d3.line()//.curve(d3.curveCardinal)
-      .x(function(d) { return t(d.date); }) // Update X mapping
-      .y(function(d) { return y(d.value); }) // Update Y mapping
+      /*svg2.selectAll(".line_temp").data([meteo_lyon]).enter()
+      .append("path")
+      .attr("class", "line_temp")
+      .attr("d", function(d) { 
+        //console.log(line(d)); 
+        return line_temp(d); })  
+      .attr("stroke", color(0))
+      .attr("fill", "none");*/
+
+      svg2.selectAll(".circle").data(meteo_lyon).enter()
+      .append("circle")
+      .attr("class", "circle")
+      .attr("r", 1)
+      .attr("cx", function(d) { 
+        
+        //console.log(d.date); 
+        return t2(d.date); })  
+      .attr("cy", function(d) { 
+        //console.log(d.date); 
+        return y_temp(d.temp); })
+      .attr("stroke", function(d){
+        if (d.date<date_min){
+          return "none";
+        }
+        return color(0);
+      })
+      .attr("fill", "none");
 
     
     svg2.append('g')
@@ -102,5 +164,13 @@ var svg2 = d3.select("#graph2").append("svg")
       .attr('transform', 'translate(0,' + 0 + ')')
       .attr('class', 'y axis')
       .call(yAxis);
+
+    svg2.append('g')
+      .attr('transform', 'translate('+imgWidth2+',' + 0 + ')')
+      .attr('class', 'y axis')
+      .call(yAxis_temp);
+
+  });
+
 
   });
